68fb57921684ea88fdb075b85b663c1ed49a7182
HRA <Hriday Aggarwal>
2022-05-12 10:48:31 +0530

    updated

--- opencv/Halt.py
+++ opencv/Halt.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-def Trafficupdate(p):
+def Trafficupdate(p,l):
     frame=cv2.imread(p)
     brg=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(brg, 90, 255, cv2.THRESH_BINARY)
@@ -14,17 +14,17 @@ def Trafficupdate(p):
         if M["m00"] !=0 :
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            print("CX : "+str(cx)+"  CY : "+str(cy))
+            print("CX : "+str(cx)+"  CY : "+str(cy)+" Cords for {0} level traffic ".format(l))
             cv2.circle(cropped_image1, (cx,cy), 5, (0,0,255), -2)
             cv2.circle(cropped_image3, (cx,cy), 5, (0,255,0), -2)
             cv2.putText(cropped_image1, "centroid is {0} , {1}".format(cx,cy), (cx - 120, cy-200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     cv2.drawContours(cropped_image1, contours, -1, (0,255,0), 1)
     return cropped_image1
 #MAIN
-a=Trafficupdate('/home/hra/Downloads/r1.png')
-b=Trafficupdate('/home/hra/Downloads/r2.png')
-c=Trafficupdate('/home/hra/Downloads/r3.png')
-d=Trafficupdate('/home/hra/Downloads/r4.png')
+a=Trafficupdate('/home/hra/Downloads/r1.png',0)
+b=Trafficupdate('/home/hra/Downloads/r2.png',1)
+c=Trafficupdate('/home/hra/Downloads/r3.png',2)
+d=Trafficupdate('/home/hra/Downloads/r4.png',3)
 while True:
         cv2.imshow("1",a)
         cv2.imshow("2",b)
